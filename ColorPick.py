61a30ea7136aebc37e69bafabcf35332a5876000
Johan Nordberg <its@johan-nordberg.com>
2011-09-20 22:30:26 +0200

    Initial commit

---
+++ ColorPick.py
@@ -0,0 +1,29 @@
+import sublime, sublime_plugin
+import subprocess
+from os import path
+
+class ColorPickCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    view = self.view
+    sel = view.sel()
+    selected = None
+
+    # get the currently selected color - if any
+    if len(sel) > 0:
+      selected = view.substr(view.word(sel[0])).strip()
+      if selected.startswith('#'): selected = selected[1:]
+
+    # get new color from picker
+    args = [path.join(sublime.packages_path(), 'ColorPick', 'bin', 'colorpick')]
+    if selected != None and len(selected) == 6:
+      args.append('-startColor')
+      args.append(selected)
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+    color = proc.communicate()[0]
+
+    # replace all regions with color
+    for region in sel:
+      word = view.word(region)
+      if view.substr(word.a - 1) == '#':
+        word = sublime.Region(word.a - 1, word.b)
+      self.view.replace(edit, word, color)
