f1625e2403d5061b3e036a237bddc752051d564d
Johan Nordberg <its@johan-nordberg.com>
2011-09-22 10:43:47 +0200

    Better detection of selected color

--- ColorPick.py
+++ ColorPick.py
@@ -2,22 +2,31 @@ import sublime, sublime_plugin
 import subprocess
 from os import path
 
+def is_hex(s):
+  try:
+    int('0x' + s, 16)
+    return True
+  except:
+    return False
+
 class ColorPickCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     view = self.view
     sel = view.sel()
-    selected = None
+    start_color = None
 
     # get the currently selected color - if any
     if len(sel) > 0:
       selected = view.substr(view.word(sel[0])).strip()
       if selected.startswith('#'): selected = selected[1:]
+      if is_hex(selected) and (len(selected) == 6 or len(selected) == 3):
+        start_color = selected
 
     # get new color from picker
     args = [path.join(sublime.packages_path(), 'ColorPick', 'bin', 'colorpick')]
-    if selected != None and (len(selected) == 6 or len(selected) == 3):
+    if start_color:
       args.append('-startColor')
-      args.append(selected)
+      args.append(start_color)
     proc = subprocess.Popen(args, stdout=subprocess.PIPE)
     color = proc.communicate()[0]
 
